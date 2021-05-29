975dc770727dc20507318b58103dfa4d4c181f9b
meghanakadambi <meghana.sahaj@gmail.com>
2021-05-29 17:29:00 -0600

    by meghana

---
+++ Mavenjavahome/src/main/java/megacademy/App.java
@@ -0,0 +1,13 @@
+package megacademy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
